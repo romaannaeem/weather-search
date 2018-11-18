e7a740138a1a8c40b8da5ba90ab3e961af68a72c
Romaan Naeem <romaan.naeem@outlook.com>
2018-11-18 15:44:37 -0500

    initial commit

---
+++ src/index.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import { Provider } from 'react-redux';
+import { createStore, applyMiddleware } from 'redux';
+import ReduxPromise from 'redux-promise'; // Resolves a promise (and send that instead) if its sent as an action payload. Amazing!
+
+import App from './components/app';
+import reducers from './reducers';
+
+const createStoreWithMiddleware = applyMiddleware(ReduxPromise)(createStore);
+
+ReactDOM.render(
+  <Provider store={createStoreWithMiddleware(reducers)}>
+    <App />
+  </Provider>,
+  document.querySelector('.container')
+);
